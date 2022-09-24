c635db66f485670412ff59fdab5f09c0eba777b5
Joel Wang <workworkjoel@gmail.com>
2022-09-24 18:20:02 +0300

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,29 +1,43 @@
 import { useState } from 'react'
+import PersonForm from './components/PersonForm'
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: '123'}
   ]) 
   const [newName, setNewName] = useState('')
-  const addName = (event) => {
-    event.preventDefault()
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  
+  const addPerson = (event) => {
 
-    setPersons(persons.concat({name: newName}))
+    const person = {name: newName, number: newNumber}
+    event.preventDefault()
+    if (persons.findIndex(person => person.name === newName) > -1 ){
+      alert(newName + " is already added to phonebook")
+      return
+    } 
+    setPersons(persons.concat(person))
+  
   }
+
+  const filterPersons = (person) => person.name.toUpperCase()
+  .indexOf(filter.toUpperCase()) > -1 
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input onChange = {(event) => setNewName(event.target.value)}/>
-        </div>
-        <div>
-          <button type="submit" onClick = {addName}>add</button>
-        </div>
-      </form>
+      <div>
+        filter shown with: <input onChange = {(event) => setFilter(event.target.value)} />
+      </div>
+      <h2>add a new</h2>
+      <PersonForm handleNameChange={(event) => setNewName(event.target.value)}
+      handleNumberChange={(event) => setNewNumber(event.target.value)}
+      handleSubmit = {addPerson}/>
       <h2>Numbers</h2>
-      {persons.map(person => <p key= {person.name}>{person.name}</p>) }
+      {persons.filter(filterPersons)
+      .map(person => <p key= {person.name}>{person.name} {person.number}</p>) }
     </div>
   )
 
