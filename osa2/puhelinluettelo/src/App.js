a3647f7e89805325dbb595c007bb6e35de762936
Joel Wang <workworkjoel@gmail.com>
2022-10-02 12:46:50 +0300

    3.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
