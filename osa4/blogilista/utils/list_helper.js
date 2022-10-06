16639b81775149b8f28db5dd973877f3ff96d51c
Joel Wang <workworkjoel@gmail.com>
2022-10-06 16:53:41 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,9 @@ const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.map(b => b.likes).reduce((sum, likes) => sum + likes, 0);
+};
 module.exports = {
-  dummy,
+  dummy,totalLikes
 };
